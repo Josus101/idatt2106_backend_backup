1ddcf4a214c9180aec9bd3ca5a5f150c8032dff0
chrihb <chrihb@stud.ntnu.no>
2025-04-21 19:18:32 +0200

    init

---
+++ src/test/java/org/ntnu/idatt2106/backend/ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ntnu.idatt2106.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
