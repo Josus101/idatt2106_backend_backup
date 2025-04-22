6c23ba2d0968a878d4fa907e60557a94b8ad9f68
Konrad <konradseime@gmail.com>
2025-04-22 14:28:57 +0200

    Feat: added password hasher

---
+++ src/main/java/org/ntnu/idatt2106/backend/security/BCryptHasher.java
@@ -0,0 +1,30 @@
+package org.ntnu.idatt2106.backend.security;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BCryptHasher {
+
+  private final BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+  private boolean stringEmpty(String password) {
+    return password == null || password.isEmpty();
+  }
+
+  public String hashPassword(String password) {
+    if (stringEmpty(password)) {
+      return null;
+    }
+    return bCryptPasswordEncoder.encode(password);
+  }
+
+  public boolean checkPassword(String password, String hash) {
+    if (stringEmpty(password) || stringEmpty(hash)) {
+      return false;
+    }
+    return bCryptPasswordEncoder.matches(password, hash);
+  }
+
+
+}
