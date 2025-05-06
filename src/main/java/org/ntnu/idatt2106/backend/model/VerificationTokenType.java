87dcfacfb87f7ef19dfc383f80b8808207b8c677
Konrad <konradseime@gmail.com>
2025-05-06 12:29:37 +0200

    feat: added token verification service

---
+++ src/main/java/org/ntnu/idatt2106/backend/model/VerificationTokenType.java
@@ -0,0 +1,14 @@
+package org.ntnu.idatt2106.backend.model;
+
+/**
+ * Enum for the different types of verification tokens.
+ *
+ * @Author Konrad Seime
+ * @since 0.2
+ */
+public enum VerificationTokenType {
+  EMAIL_VERIFICATION,
+  PASSWORD_RESET,
+  ADMIN_VERIFICATION,
+  TWO_FACTOR_AUTHENTICATION
+}
