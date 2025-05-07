87ed00b6d59a15dfa422cc5fa01a8af46bc5d01f
Konrad <konradseime@gmail.com>
2025-05-07 14:44:30 +0200

    fixed docs

--- src/main/java/org/ntnu/idatt2106/backend/dto/user/PasswordResetRequest.java
+++ src/main/java/org/ntnu/idatt2106/backend/dto/user/PasswordResetRequest.java
@@ -6,6 +6,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Data Transfer Object used for password reset requests.
+ * Contains the new password for the user.
+ * @Author Konrad Seime
+ * @since 0.2
+ */
 @Getter
 @Setter
 @NoArgsConstructor
